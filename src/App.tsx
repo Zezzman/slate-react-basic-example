2629b236b79de41091df3fdc58130a2fdfdcad4e
Francois Le Roux <francois@datanova.com.au>
2022-10-25 12:08:25 +1000

    implemented theme with scss typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Fab from '@mui/material/Fab';
+import Menu from '@mui/icons-material/Menu';
+import { Portal } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<Box position={'relative'}>
+    <Fab color="inherit" aria-label="add" sx={{ position: 'absolute', top: '10px', left: '10px' }}>
+      <Menu />
+      <Portal container={document.body}>
+        <Box className='fab-menu'>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            MUI
+          </Typography>
+        </Box>
+      </Portal>
+    </Fab>
+    <Container>
+      <Box sx={{ py: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App example with TypeScript
+        </Typography>
+      </Box>
+    </Container>
+  </Box>
   );
 }
 
---
+++ src/themes/typography.ts
@@ -0,0 +1,7 @@
+import { TypographyOptions } from "@mui/material/styles/createTypography";
+
+export default function themeTypography(theme: any): TypographyOptions {
+  return {
+   
+  };
+}
