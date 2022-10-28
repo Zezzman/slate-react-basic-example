e288198d11ace7e2c23a77ad8df9244f3c8b60a7
Francois Le Roux <francois@datanova.com.au>
2022-10-28 11:50:16 +1000

    converted all js to ts

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import App from './App';
 import theme from './themes/theme';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
